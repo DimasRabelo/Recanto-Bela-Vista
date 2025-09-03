efc8847ddb3603523652b2aabd40b2a0c8503ba7
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-09-03 17:18:18 -0300

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",        // todos os arquivos HTML na raiz
+    "./**/*.html"      // todos os HTML em subpastas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
